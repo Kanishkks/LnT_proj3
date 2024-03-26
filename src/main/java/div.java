9fe66a9078b29b8baf8ea10b7953c739792c92db
Kanishk K S <68496351+Kanishkks@users.noreply.github.com>
2024-03-26 17:34:12 +0530

    Update div.java

--- src/main/java/div.java
+++ src/main/java/div.java
@@ -2,7 +2,7 @@
 public class div {
 	 public static double div(double num1, double num2) {
 	        if (num2 != 0) {
-	            return 0;
+	            return num1/num2;
 	        } else {
 	            System.out.println("Error: Cannot divide by zero");
 	            return Double.NaN; // Not a Number
